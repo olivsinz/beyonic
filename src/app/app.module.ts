a8d7e6e29499262077f965230efa1ab52410e846
Oliversinz <zinsouoliviers@gmail.com>
2022-10-07 17:21:59 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { HistoryComponent } from './history/history.component';
+// import { NavmenuComponent } from './navmenux/navmenu.component';
+import { NavItemComponent } from './menu/nav-item/nav-item.component';
+import { NavComponent } from './menu/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TransferComponent,
+    HistoryComponent,
+    // NavmenuComponent,
+    NavItemComponent,
+    NavComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
